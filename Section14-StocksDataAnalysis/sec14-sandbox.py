80bed8db6435f83d6f6339c585545ae8d3715a98
timvirga <timvirga@gmail.com>
2022-11-20 11:34:45 +1100

    daily-returns-multiple-cols

--- Section14-StocksDataAnalysis/sec14-sandbox.py
+++ Section14-StocksDataAnalysis/sec14-sandbox.py
@@ -15,3 +15,27 @@ os.getcwd()
 
 stocks_df = pd.read_csv('stock.csv')
 stocks_df
+
+# ---------------------------------------------- # 
+df = stocks_df.copy()
+# Sandbox 
+
+# Let's define a function to calculate stocks daily returns (for all stocks) 
+
+def daily_return(df):
+    df_daily_returns = df.copy()
+    # loop through cols
+    for i in df.columns[1:]:
+        # nest loop through rows to calc daily % difference  
+        for j in range(1,len(df)): # second indexer j so we can reference two rows simultaneously 
+            # calculation is:
+            # df[stock_index][row_index]
+            #  - df[stock_index][prev_row_index] 
+            # / df[stock_index][prev_row_index] 
+            # * 100
+            df_daily_returns[i][j] = ( ( df[i][j] - df[i][j-1] ) / df[i][j-1] ) * 100
+        # because we skipped index 0 in loop need to specify index 0 as 0
+        df_daily_returns[i][0] = 0
+    return df_daily_returns
+
+daily_return(stocks_df)
