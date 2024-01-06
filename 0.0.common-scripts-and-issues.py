df470a295e0e40ed3d38a142b92a987e3698af0e
timvirga <timvirga@gmail.com>
2024-01-06 12:20:25 +1100

    s

--- 0.0.common-scripts-and-issues.py
+++ 0.0.common-scripts-and-issues.py
@@ -3,6 +3,7 @@
 
 
 # using virtual environments with github https://www.youtube.com/watch?v=6W6iY7uUu34 
+
 # After completing work in a venv and ready to upload to VCS
 # pip freeze > requirements.txt will create a requirements.txt file with your currently installed packages into the dir
 # add the requirements.txt along with 
@@ -10,10 +11,19 @@
 # after a venv has been created (ideally in the github repo) clone repo then activate env with ~PATHTOVENV/VENVNAME/Script/activate.bat
 # the name of the venv should show in command prompt at the beginning of the cmd line
 
-# $ git clone <Project A>  # Cloning project repository
+
+## Create and clone project repository from github
+# add readme and python .gitignore template
+# Open git bash terminal (through vscode or wherever) and $ git clone <Project A>  
+
+## create a virtual environment ##
+
 # $ cd <Project A> # Enter to project directory
-# $ python -m venv my_venv # If not created, creating virtualenv
-# $ source ./my_venv/bin/activate # Activating virtualenv
+# open anaconda cmd prompt and create a venv in the working dir with: python -m venv venvname ## or create from python installed on os using below line
+#### $ python -m venv my_venv # If not created, creating virtualenv ####
+# cd to $ ./my_venv/Scripts/ #
+# Activate virtualenv with activate.bat
+# cd .. back to working dir
 # upgrade pip in new environment (this command on windows) python -m pip install --upgrade pip
 # (my_venv)$ pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org -r requirements.txt # Installing dependencies from requirements.txt
 # (my_venv)$ deactivate # When you want to leave virtual environment
